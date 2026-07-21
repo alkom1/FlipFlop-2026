15acb0ae54fa9fc26354e590f3c2e1b5bf25293a
alkom1 <17685430+alkom1@users.noreply.github.com>
2026-07-22 00:51:12 +0200

    puzzle 9 part 3

--- 9.py
+++ 9.py
@@ -1,14 +1,13 @@
 from collections import defaultdict
 from itertools import count
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from typing import Dict, Generator, List, Literal, Set, Tuple
 import heapq
-from math import inf, sqrt
-import cProfile
+from math import inf
 from functools import cache
 Point = Tuple[int, int]
 
-with open("9.test.txt", "rt") as file:
+with open("9.in.txt", "rt") as file:
 	INPUT = [list(x.strip()) for x in file]
 	WIDTH = len(INPUT[0])
 	HEIGHT = len(INPUT)
@@ -25,11 +24,10 @@ class State:
 	def new(p: Point) -> 'State':
 		return State(p, (-1, -1), (-1, -1))
 	
-	@staticmethod
-	def clone(other: 'State', p: Point|None=None, r:Point|None=None, b:Point|None=None) -> 'State':
-		p = p if p is not None else other.position
-		r = r if r is not None else other.red
-		b = b if b is not None else other.blue
+	def clone(self, p: Point|None=None, r:Point|None=None, b:Point|None=None) -> 'State':
+		p = p if p is not None else self.position
+		r = r if r is not None else self.red
+		b = b if b is not None else self.blue
 		if r > b:
 			r, b = b, r
 		return State(p, r, b)
@@ -52,6 +50,14 @@ def isRoom(p: Point) -> bool:
 	return INPUT[p[1]][p[0]] in ('.', 'S', 'E')
 
 @cache
+def wallNextToMe(p: Point) -> bool:
+	x, y = p
+	for n in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)):
+		if not isRoom(n):
+			return True
+	return False
+
+@cache
 def findFurthestAway(a: Point, minDepth: int = 2) -> List[Point]:
 	results: Set[Point] = set()
 	for d in DIRECTIONS:
@@ -67,30 +73,29 @@ def findFurthestAway(a: Point, minDepth: int = 2) -> List[Point]:
 				results.add(point)
 	return list(results)
 
-def get_neighbors(initial: State) -> Generator[State]:
+def get_neighbors(initial: State) -> Generator[Tuple[int, State]]:
 	# possible actions:
 	#  1. move in cardinal directions if there isnt a wall (1+2+3)
 	for d in DIRECTIONS:
 		point = (initial.position[0] + d[0], initial.position[1] + d[1])
 		if isRoom(point):
-			yield State.clone(initial, p = point)
+			yield 1, initial.clone(p = point)
 	#  2. move across the whole hallway (2)
 	if PART == 2:
-		yield from (State.clone(initial, p = point) for point in findFurthestAway(initial.position))
-	#  3. shoot red/blue portal in cardinal directions (3)
-	if PART == 3:
-		yield from (State.clone(initial, r = point) for point in findFurthestAway(initial.position, minDepth=0))
-		yield from (State.clone(initial, b = point) for point in findFurthestAway(initial.position, minDepth=0))
-	#  4. move through the portal if its on the current tile (3)
-	if PART == 3 and initial.red != (-1, -1) and initial.blue != (-1, -1) and initial.red != initial.blue:
-		if initial.position == initial.red:
-			yield State.clone(initial, p = initial.blue)
-		if initial.position == initial.blue:
-			yield State.clone(initial, p = initial.red)
-
+		yield from ((1, initial.clone(p = point)) for point in findFurthestAway(initial.position))
+	#  3. we're not actually shooting portals - we're faking it
+	if not PART == 3 or not wallNextToMe(initial.position):
+		return
+	if initial.position == initial.red:
+		yield from ((2, initial.clone(p = point, b = point)) for point in findFurthestAway(initial.position))
+	elif initial.position == initial.blue:
+		yield from ((2, initial.clone(p = point, r = point)) for point in findFurthestAway(initial.position))
+	else:
+		yield from ((3, initial.clone(p = point, r = point, b = initial.position)) for point in findFurthestAway(initial.position))
 
 # A*
 def astar():
+	@cache
 	def walking_distance(a: Point, b: Point) -> int:
 		return abs(a[0] - b[0]) + abs(a[1] - b[1])
 	def h(s: State) -> float:
@@ -103,7 +108,7 @@ def astar():
 		)
 
 	def reconstruct_path(current: State):
-		print(g_score[current])
+		print("part", PART, g_score[current])
 
 	counter = count()
 	initial_state = State.new(start)
@@ -122,17 +127,12 @@ def astar():
 		if current.position == end:
 			reconstruct_path(current)
 			break
-		for n in get_neighbors(current):
-			tentative_g_score = g_score[current] + 1
+		for w, n in get_neighbors(current):
+			tentative_g_score = g_score[current] + w
 			if tentative_g_score < g_score[n]:
 				came_from[n] = current
 				g_score[n] = tentative_g_score
 				f_score[n] = tentative_g_score + h(n)
 				heapq.heappush(open_set, (f_score[n], next(counter), n))
 
-cProfile.run('astar()', sort="tottime")
-print("end")
-print("findFurthestAway", findFurthestAway.cache_info())
-
-# PART 1: 1338
-# PART 2: 491
+astar()
